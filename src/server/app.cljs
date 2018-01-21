244267ab2d71397b5fdc921964f54a8c2304852e
Will Acton <will@willacton.com>
2018-01-21 13:51:30 -0800

    hot reload works

--- src/server/app.cljs
+++ src/server/app.cljs
@@ -10,17 +10,4 @@
 
 (.on js/process "uncaughtException" #(js/console.error %))
 
-(defonce server-ref
-  (volatile! nil))
-
 (def init core/server)
-
-(defn start [& cli-args]
-  (vreset! server-ref (core/server)))
-
-(defn stop [done]
-  (when-some [srv @server-ref]
-    (.close srv
-            (fn [err]
-              (js/console.log "stop completed" err)
-              (done)))))
